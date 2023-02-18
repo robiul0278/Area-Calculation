0249b851f5d91045780bc948ecaa6d4e276fbfa6
robiul0278 <robiul0278@gmail.com>
2023-02-18 23:09:17 +0600

    comment add

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
-// Area of Triangle --------
 
+
+document.getElementById('pentagon').addEventListener('click', function(){
+    const name = document.getElementById('pentagon-name').innerText;
+    const inputBase = document.getElementById('pentagon-p')
+    const inputValueB= inputBase.value;
+    const inputHight = document.getElementById('pentagon-b')
+    const inputValueH = inputHight.value;
+
+    let pentagonArea = 0.5 * parseInt(inputValueB) * parseInt(inputValueH) ;
+    
+    displayData(name, pentagonArea)
+})
 document.getElementById('triangle').addEventListener('click', function(){
     const name = document.getElementById('triangle-name').innerText;
     const inputBase = document.getElementById('t-base')
@@ -13,7 +24,21 @@ document.getElementById('triangle').addEventListener('click', function(){
     displayData(name, resultArea)
 })
 
-// Area of Rectangle ----------
+
+document.getElementById('rhombus').addEventListener('click', function(){
+    const name = document.getElementById('rhombus-name').innerHTML;
+    const inputDataA = document.getElementById('r-diagonal1')
+    const inputValueA= inputDataA.value;
+    const inputDataB = document.getElementById('r-diagonal2')
+    const inputValueB = inputDataB.value;
+
+    let rhombusArea = 0.5 * parseInt(inputValueA) * parseInt(inputValueB) ;
+
+    displayData(name, rhombusArea)
+})
+
+
+
 
 document.getElementById('rectangle').addEventListener('click', function(){
     const name = document.getElementById('rectangle-name').innerHTML;
@@ -26,32 +51,13 @@ document.getElementById('rectangle').addEventListener('click', function(){
 
     displayData(name, rectangleArea)
 })
-// Area of Parallelogram ----------
 
-document.getElementById('parallelogram').addEventListener('click', function(){
-    const name = document.getElementById('parallelogram-name').innerHTML;
-    const input1 = document.getElementById('parallelogram-b')
-    const inputValueW= input1.value;
-    const input2 = document.getElementById('parallelogram-h')
-    const inputValueL = input2.value;
 
-    let parallelogramArea = parseInt(inputValueW) * parseInt(inputValueL) ;
 
-    displayData(name, parallelogramArea)
-})
-// Area of Rhombus ----------
 
-document.getElementById('rhombus').addEventListener('click', function(){
-    const name = document.getElementById('rhombus-name').innerHTML;
-    const input1 = document.getElementById('r-diagonal1')
-    const inputValueA= input1.value;
-    const input2 = document.getElementById('r-diagonal2')
-    const inputValueB = input2.value;
 
-    let rhombusArea = 0.5 * parseInt(inputValueA) * parseInt(inputValueB) ;
 
-    displayData(name, rhombusArea)
-})
+
 
 // Output Result of triangle or Rhombus ==========
 
@@ -65,3 +71,15 @@ function displayData(name, resultArea){
     `;
     tableContainer.appendChild(tr);
 }
+document.getElementById('parallelogram').addEventListener('click', function(){
+    const name = document.getElementById('parallelogram-name').innerHTML;
+    const inputB = document.getElementById('parallelogram-b')
+    const inputValueB = inputB.value;
+    const inputH = document.getElementById('parallelogram-h')
+    const inputValueH = inputH.value;
+
+    let parallelogramArea = parseInt(inputValueB) * parseInt(inputValueH) ;
+
+    displayData(name, parallelogramArea)
+})
+
