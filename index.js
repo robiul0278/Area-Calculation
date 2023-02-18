ada412be755cdc07644810598e2fa75a6fc04073
robiul0278 <robiul0278@gmail.com>
2023-02-18 23:24:25 +0600

    math.pi added

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 
+// Area of Triangle -----
 
-
-document.getElementById('pentagon').addEventListener('click', function(){
-    const name = document.getElementById('pentagon-name').innerText;
-    const inputBase = document.getElementById('pentagon-p')
-    const inputValueB= inputBase.value;
-    const inputHight = document.getElementById('pentagon-b')
-    const inputValueH = inputHight.value;
-
-    let pentagonArea = 0.5 * parseInt(inputValueB) * parseInt(inputValueH) ;
-    
-    displayData(name, pentagonArea)
-})
 document.getElementById('triangle').addEventListener('click', function(){
     const name = document.getElementById('triangle-name').innerText;
     const inputBase = document.getElementById('t-base')
@@ -24,6 +13,37 @@ document.getElementById('triangle').addEventListener('click', function(){
     displayData(name, resultArea)
 })
 
+
+
+// Area of Rectangle -----
+
+document.getElementById('rectangle').addEventListener('click', function(){
+    const name = document.getElementById('rectangle-name').innerHTML;
+    const input1 = document.getElementById('rectangle-w')
+    const inputValueW= input1.value;
+    const input2 = document.getElementById('rectangle-l')
+    const inputValueL = input2.value;
+
+    let rectangleArea = parseInt(inputValueW) * parseInt(inputValueL) ;
+
+    displayData(name, rectangleArea)
+})
+
+// Area of Parallelogram -----
+
+document.getElementById('parallelogram').addEventListener('click', function(){
+    const name = document.getElementById('parallelogram-name').innerHTML;
+    const inputB = document.getElementById('parallelogram-b')
+    const inputValueB = inputB.value;
+    const inputH = document.getElementById('parallelogram-h')
+    const inputValueH = inputH.value;
+
+    let parallelogramArea = parseInt(inputValueB) * parseInt(inputValueH) ;
+
+    displayData(name, parallelogramArea)
+})
+
+// Area of Rhombus -----
 
 document.getElementById('rhombus').addEventListener('click', function(){
     const name = document.getElementById('rhombus-name').innerHTML;
@@ -38,24 +58,35 @@ document.getElementById('rhombus').addEventListener('click', function(){
 })
 
 
+// Area of Pentagon -----
 
 
-document.getElementById('rectangle').addEventListener('click', function(){
-    const name = document.getElementById('rectangle-name').innerHTML;
-    const input1 = document.getElementById('rectangle-w')
-    const inputValueW= input1.value;
-    const input2 = document.getElementById('rectangle-l')
-    const inputValueL = input2.value;
+document.getElementById('pentagon').addEventListener('click', function(){
+    const name = document.getElementById('pentagon-name').innerText;
+    const inputP = document.getElementById('pentagon-p')
+    const inputValueB= inputP.value;
+    const inputB = document.getElementById('pentagon-b')
+    const inputValueH = inputB.value;
 
-    let rectangleArea = parseInt(inputValueW) * parseInt(inputValueL) ;
-
-    displayData(name, rectangleArea)
+    let pentagonArea = 0.5 * parseInt(inputValueB) * parseInt(inputValueH) ;
+    
+    displayData(name, pentagonArea)
 })
 
 
+// Area of Ellipse -----
 
+document.getElementById('ellipse').addEventListener('click', function(){
+    const name = document.getElementById('ellipse-name').innerText;
+    const inputA = document.getElementById('ellipse-a')
+    const inputValueA= inputA.value;
+    const inputB = document.getElementById('ellipse-b')
+    const inputValueB = inputB.value;
 
-
+    let pentagonArea = math.pi * parseInt(inputValueA) * parseInt(inputValueB) ;
+    
+    displayData(name, pentagonArea)
+})
 
 
 
@@ -71,15 +102,4 @@ function displayData(name, resultArea){
     `;
     tableContainer.appendChild(tr);
 }
-document.getElementById('parallelogram').addEventListener('click', function(){
-    const name = document.getElementById('parallelogram-name').innerHTML;
-    const inputB = document.getElementById('parallelogram-b')
-    const inputValueB = inputB.value;
-    const inputH = document.getElementById('parallelogram-h')
-    const inputValueH = inputH.value;
-
-    let parallelogramArea = parseInt(inputValueB) * parseInt(inputValueH) ;
-
-    displayData(name, parallelogramArea)
-})
 
